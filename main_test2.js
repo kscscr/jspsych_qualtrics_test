3de5902dbdd59f347ffe9b95e060fae21ee06458
kscscr <63399643+kscscr@users.noreply.github.com>
2025-10-08 21:14:32 +0900

    Add files via upload

--- main_test2.js
+++ main_test2.js
@@ -23,6 +23,60 @@ var instruction_pronounIAT_main01 = {
 
 
 
+//13. 代名詞IATの本番試行（私 + 好ましい言葉 / 私でない + 好ましくない言葉）
+
+var trial_pronounIAT_main01 = {
+  timeline: [
+    {
+      type: 'iat-html',
+      stimulus: jsPsych.timelineVariable('stimulus'),
+      stim_key_association: jsPsych.timelineVariable('stim_key_association'),
+      html_when_wrong: '<span style="color: red; font-size: 80px">×</span>',
+      bottom_instructions: '<p>間違えるとバツが表示されるので、押したキーと反対のキーを押してください</p>',
+      force_correct_key_press: true,
+      display_feedback: true,
+      left_category_key: 'E',
+      right_category_key: 'I',
+      left_category_label: ['私', '好ましい言葉'],
+      right_category_label: ['私でない', '好ましくない言葉'],
+      response_ends_trial: true,
+    }
+  ],
+
+  //IATで使う刺激
+  timeline_variables: [
+    {stimulus: "私", stim_key_association: "left"},
+    {stimulus: "私の", stim_key_association: "left"},
+    {stimulus: "私に", stim_key_association: "left"},
+    {stimulus: "自分の", stim_key_association: "left"},
+    {stimulus: "自分に", stim_key_association: "left"},
+
+    {stimulus: "彼らは", stim_key_association: "right"},
+    {stimulus: "彼らの", stim_key_association: "right"},
+    {stimulus: "他人の", stim_key_association: "right"},
+    {stimulus: "他の人", stim_key_association: "right"},
+    {stimulus: "他人は", stim_key_association: "right"},
+
+    {stimulus: "温かい", stim_key_association: "left"},
+    {stimulus: "幸せ", stim_key_association: "left"},
+    {stimulus: "楽しい", stim_key_association: "left"},
+    {stimulus: "嬉しい", stim_key_association: "left"},
+    {stimulus: "心地よい", stim_key_association: "left"},
+
+    {stimulus: "苦しい", stim_key_association: "right"},
+    {stimulus: "醜い", stim_key_association: "right"},
+    {stimulus: "痛い", stim_key_association: "right"},
+    {stimulus: "恥ずかしい", stim_key_association: "right"},
+    {stimulus: "汚い", stim_key_association: "right"}
+
+    
+  ],
+  randomize_order: true,
+  post_trial_gap: 50,
+  repetitions: 1
+};
+
+
 //14.　（入れ替え）代名詞IATの本番試行（私 + 好ましくない言葉 / 私でない + 好ましい言葉）の教示
 var instruction_pronounIAT_main02 = {
   type: 'html-keyboard-response',
@@ -39,12 +93,93 @@ var instruction_pronounIAT_main02 = {
   choices:[' '],
 };
 
+//15.　（入れ替え）代名詞IATの本番試行（私 + 好ましくない言葉 / 私でない + 好ましい言葉）
+var trial_pronounIAT_main02 = {
+  timeline: [
+    {
+      type: 'iat-html',
+      stimulus: jsPsych.timelineVariable('stimulus'),
+      stim_key_association: jsPsych.timelineVariable('stim_key_association'),
+      html_when_wrong: '<span style="color: red; font-size: 80px">×</span>',
+      bottom_instructions: '<p>間違えるとバツが表示されるので、押したキーと反対のキーを押してください</p>',
+      force_correct_key_press: true,
+      display_feedback: true,
+      left_category_key: 'E',
+      right_category_key: 'I',
+      left_category_label: ['私', '好ましくない言葉'],
+      right_category_label: ['私でない', '好ましい言葉'],
+      response_ends_trial: true,
+    }
+  ],
+
+  //IATで使う刺激
+  timeline_variables: [
+    {stimulus: "私", stim_key_association: "left"},
+    {stimulus: "私の", stim_key_association: "left"},
+    {stimulus: "私に", stim_key_association: "left"},
+    {stimulus: "自分の", stim_key_association: "left"},
+    {stimulus: "自分に", stim_key_association: "left"},
+
+    {stimulus: "彼らは", stim_key_association: "right"},
+    {stimulus: "彼らの", stim_key_association: "right"},
+    {stimulus: "他人の", stim_key_association: "right"},
+    {stimulus: "他の人", stim_key_association: "right"},
+    {stimulus: "他人は", stim_key_association: "right"},
+
+    {stimulus: "苦しい", stim_key_association: "left"},
+    {stimulus: "醜い", stim_key_association: "left"},
+    {stimulus: "痛い", stim_key_association: "left"},
+    {stimulus: "恥ずかしい", stim_key_association: "left"},
+    {stimulus: "汚い", stim_key_association: "left"},
+
+    {stimulus: "温かい", stim_key_association: "right"},
+    {stimulus: "幸せ", stim_key_association: "right"},
+    {stimulus: "楽しい", stim_key_association: "right"},
+    {stimulus: "嬉しい", stim_key_association: "right"},
+    {stimulus: "心地よい", stim_key_association: "right"}
+    
+  ],
+  randomize_order: true,  
+  post_trial_gap: 50,
+  repetitions: 1
+};
+
+
+
+
+// 16. IAT課題の終了メッセージ
+var endmessage = {
+    type: 'instructions',
+    pages: ["<p>この課題はこれで終了になります</p>"+"<p>「次へ」をクリックして次へ進んでください</p>"
+    ],
+    allow_backward: false,
+    show_clickable_nav: true,
+    button_label_next: "次へ"
+    
+};
+
 
 /* 実験の提示の順番 */
-var timeline2 = [];
+var timeline = [];
 
 
-timeline2.push(instruction_pronounIAT_main01);
-timeline2.push(instruction_pronounIAT_main02);
 
+timeline.push({
+  type: 'fullscreen',
+  fullscreen_mode: true
+});
+
+timeline.push(instruction_pronounIAT_main01);
+timeline.push(trial_pronounIAT_main01);
+timeline.push(instruction_pronounIAT_main02);
+timeline.push(trial_pronounIAT_main02);
+
+
+
+timeline.push({
+  type: 'fullscreen',
+  fullscreen_mode: false
+});
+
+timeline.push(endmessage);
 
